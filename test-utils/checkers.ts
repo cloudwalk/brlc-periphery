947e088d08ea4b854e8c10e67b7c0bfddc0dfc27
Evgenii Zaitsev <evgenii@cloudwalk.io>
2023-12-19 22:15:26 +0700

    Extend the helper test scripts

--- test-utils/checkers.ts
+++ test-utils/checkers.ts
@@ -1,23 +1,47 @@
 import { expect } from "chai";
 
-function checkEventField(fieldName: string, expectedValue: any): (value: any) => boolean {
+export interface EventFieldCheckingOptions {
+  showValuesInErrorMessage?: boolean
+  caseInsensitiveComparison?: boolean
+}
+
+function checkEventField(
+  fieldName: string,
+  expectedValue: any,
+  options: EventFieldCheckingOptions = {}
+): (value: any) => boolean {
   const f = function (value: any): boolean {
-    expect(value).to.equal(
-      expectedValue,
-      `The "${fieldName}" field of the event is wrong`
-    );
+    let errorMessage = `The "${fieldName}" field of the event is wrong`;
+    if (options.showValuesInErrorMessage) {
+      errorMessage += ` (actual: ${value} ; expected: ${expectedValue})`;
+    }
+    if (options.caseInsensitiveComparison) {
+      value = value.toString().toLowerCase();
+      expectedValue = expectedValue.toString().toLowerCase();
+    }
+    expect(value).to.equal(expectedValue, errorMessage);
     return true;
   };
   Object.defineProperty(f, "name", { value: `checkEventField_${fieldName}`, writable: false });
   return f;
 }
 
-function checkEventFieldNotEqual(fieldName: string, notExpectedValue: any): (value: any) => boolean {
+function checkEventFieldNotEqual(
+  fieldName: string,
+  notExpectedValue: any,
+  options: EventFieldCheckingOptions = {}
+): (value: any) => boolean {
   const f = function (value: any): boolean {
-    expect(value).not.to.equal(
-      notExpectedValue,
-      `The "${fieldName}" field of the event is wrong because it is equal ${notExpectedValue} but should not`
-    );
+    let errorMessage =
+      `The "${fieldName}" field of the event is wrong because it is equal ${notExpectedValue} but should not`;
+    if (options.showValuesInErrorMessage) {
+      errorMessage += ` (actual: ${value} ; not expected: ${notExpectedValue})`;
+    }
+    if (options.caseInsensitiveComparison) {
+      value = value.toString().toLowerCase();
+      notExpectedValue = notExpectedValue.toString().toLowerCase();
+    }
+    expect(value).not.to.equal(notExpectedValue, errorMessage);
     return true;
   };
   Object.defineProperty(f, "name", { value: `checkEventFieldNot_${fieldName}`, writable: false });
