6caa230b2d7124e460bf389a41539ec4d03b56d6
Evgenii Zaitsev <evgenii@cloudwalk.io>
2025-06-12 22:45:39 +0700

    docs: improve a comment

--- contracts/DispatcherStorage.sol
+++ contracts/DispatcherStorage.sol
@@ -12,7 +12,7 @@ abstract contract DispatcherStorage {
      * @dev This empty reserved space is put in place to allow future versions to add new
      * variables without shifting down storage in the inheritance chain.
      *
-     * IMPORTANT! For actual storage use, implement through EIP-7201 namespaces.
+     * IMPORTANT! For actual storage use, implement it through EIP-7201 namespaces.
      */
     uint256[50] private __gap;
 }
